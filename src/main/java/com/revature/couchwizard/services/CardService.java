0add64bf1967385c1f40c2932e364c72306415b3
William <Dragonimi1337@gmail.com>
2021-11-24 07:59:39 -0800

    Added Card service from quizzard example. Removed flashcard related references and commented out issues with DAO and implementation. Needs work but builds

---
+++ src/main/java/com/revature/couchwizard/services/CardService.java
@@ -0,0 +1,42 @@
+package com.revature.couchwizard.services;
+
+import com.revature.couchwizard.models.card;
+
+import java.util.List;
+import com.revature.couchwizard.daos.cardDAO;
+
+public class CardService {
+
+    private final cardDAO cardDAO;
+
+    public CardService(cardDAO cardDAO) {
+        this.cardDAO = cardDAO;
+    }
+
+    public List<card> findAllCards() {
+        //TODO Implement Maybe
+       // return cardDAO.findAll();
+        return null;
+    }
+
+    public void createNewCard(card newCard) {
+
+        if (!isCardValid(newCard)) {
+            //TODO Exception Logic
+            //throw new InvalidRequestException("Invalid card information values provided!");
+        }
+
+        card addedCard = cardDAO.save(newCard);
+
+        if (addedCard == null) {
+            //TODO Exception Logic
+            //throw new ResourcePersistenceException("The card could not be persisted to the datasource!");
+        }
+
+    }
+
+    public boolean isCardValid(card card) {
+            //TODO Write Valid Card Checks
+        return true;
+    }
+}
