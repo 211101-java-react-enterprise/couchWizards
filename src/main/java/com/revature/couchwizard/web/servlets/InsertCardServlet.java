1ad604a0f92eae9bb8d172ced06366c7a3cafaff
William <Dragonimi1337@gmail.com>
2021-11-27 18:01:07 -0800

    Added FindAll methods in Cardservice and DAO. Removed webservlet Annotations, ask Wezley

--- src/main/java/com/revature/couchwizard/web/servlets/InsertCardServlet.java
+++ src/main/java/com/revature/couchwizard/web/servlets/InsertCardServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/save")
+
 public class InsertCardServlet extends HttpServlet {
 
     private final CardService cardService;
--- src/main/java/com/revature/couchwizard/web/servlets/UpdateCardServlet.java
+++ src/main/java/com/revature/couchwizard/web/servlets/UpdateCardServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/update")
+
 public class UpdateCardServlet extends HttpServlet {
 
     private final CardService cardService;
